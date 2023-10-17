b1d7c84122c8356cb720e811171deb17cecf4cb2
HoKeiL <hokeileung1106@gmail.com>
2023-10-17 20:16:01 +0100

    SQL for orderpfplay

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS teams RESTRICT;
 
 CREATE TABLE teams (
 id SERIAL PRIMARY KEY,
-  teamname TEXT NOT NULL,
+  teamname TEXT NOT NULL UNIQUE,
   teamcaptain TEXT NOT NULL,
   teamplayer1 TEXT NOT NULL,
   teamplayer2 TEXT NOT NULL,
@@ -14,5 +14,34 @@ id SERIAL PRIMARY KEY,
 INSERT INTO teams(teamname, teamcaptain, teamplayer1,teamplayer2,teamplayer3,teamplayer4,teamplayer5,teamplayer6) VALUES('test team', 'Gav', 'Kelvin','Adrian', 'James', 'HK','Vanessa','Henry') RETURNING *,
             
 SELECT * FROM teams;
+
+DROP TABLE IF EXISTS
+  orderOfPlay RESTRICT;
+
+CREATE TABLE
+  orderOfPlay (
+    id SERIAL PRIMARY KEY,
+    teamname TEXT REFERENCES teams(teamname),
+    Game1Player1 TEXT NOT NULL,
+    Game1Player2 TEXT NOT NULL,
+    Game2Player1 TEXT NOT NULL,
+    Game2Player2 TEXT NOT NULL,
+    Game3Player1 TEXT NOT NULL,
+    Game3Player2 TEXT NOT NULL,
+    Game4Player1 TEXT NOT NULL,
+    Game4Player2 TEXT NOT NULL,
+    Game5Player1 TEXT NOT NULL,
+    Game5Player2 TEXT NOT NULL,
+    Game6Player1 TEXT NOT NULL,
+    Game6Player2 TEXT NOT NULL,
+    Game7Player1 TEXT NOT NULL,
+    Game7Player2 TEXT NOT NULL
+  );
+  
+  SELECT * FROM orderofplay
+INSERT INTO orderOfPlay(teamname, Game1Player1, Game1Player2, Game2Player1, Game2Player2, Game3Player1, Game3Player2, Game4Player1, Game4Player2, Game5Player1, Game5Player2,Game6Player1,Game6Player2, Game7Player1,Game7Player2) VALUES() RETURNING *,
+            
+SELECT * FROM orderOfPlay;
+
 
 
